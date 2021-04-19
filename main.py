e6ac914749a90bd1c75f0c500eced8444624a70e
dohako <dohako@inbox.ru>
2021-04-19 13:00:20 +0300

    REF all scripts

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from d_utils import get_psql_env
-from psycopg2 import connect
+from base_handler import BaseHandler
 
 
 def main():
-    database, user, password, host, port = get_psql_env()
-    conn = connect(database='adjust', user=user, password=password, host=host, port=port)
-    conn.autocommit = True
-    cursor = conn.cursor()
-
-def execute_sql(db_cursor, line):
+    base = BaseHandler('adjust')
     sql = 'select * from data;'
-    db_cursor.execute(sql)
-    first_level = db_cursor.fetchall()
-    print(first_level[-1])
+    string = base.get_data(sql)
+    print(string[1])
 
 
 if __name__ == '__main__':
--- web_api.py
+++ web_api.py
@@ -2,40 +2,52 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from os.path import abspath
 
 CSV_DIR_PATH = abspath(".\\tmp\\separated csv\\")
+TEST_HTTP = "http://127.0.0.1:5000/show?channel,country,sum-impressions_as_impressions,sum-clicks_as_clicks&date:2017-06-01&group:channel,country&order:clicks"
 
 
 class ServiceHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text')
         get_request = self.requestline
         endpoint = get_request.split('/')[1].split(' ')[0]
-        if endpoint == '':
-            self.end_headers()
-            self.wfile.write(bytes("Hello, we have some products, check it out (example "
-                                   "http://127.0.0.1:5000/product?sku=WP260qJAo6&rank=0.9)", 'utf-8'))
-            return
-        if '?' not in endpoint:
-            self.end_headers()
-            self.wfile.write(bytes("Please, choose at least sku, rank is optional", 'utf-8'))
-            return
-        args = endpoint.split('?')[1].split('&')
 
-        for arg in args:
-            if "sku" in arg:
-                sku = arg.split('=')[1]
-            elif "rank" in arg:
-                rank = arg.split('=')[1]
-        if isinstance(sku, type(None)):
-            self.end_headers()
-            self.wfile.write(bytes("Sorry, but sku is obligatory, rank is optional", 'utf-8'))
-            return
+        result = self.args_handler(endpoint)
 
         self.end_headers()
         self.wfile.write(bytes(f"{result}", 'utf-8'))
         return
 
+    def args_handler(self, line_to_parse:str):
+        result = ''
+        print(line_to_parse)
+        if line_to_parse == '':
+            result = f"Hello, we have some products, check it out.\n(example {TEST_HTTP})"
+        elif 'show' not in line_to_parse:
+            result = f"There is only one endpoint, stick to it please.\n(example {TEST_HTTP})"
+        elif '?' not in line_to_parse:
+            result = "Please, choose some parameters"
+        else:
+            args = line_to_parse.split('?')[1].split('&')
 
-if __name__ == '__main__':
+            for arg in args:
+                if "sku" in arg:
+                    sku = arg.split('=')[1]
+                elif "rank" in arg:
+                    rank = arg.split('=')[1]
+            if isinstance(sku, type(None)):
+                result = "Sorry, but sku is obligatory, rank is optional"
+        return result
+
+    def shut_down(self):
+        self.server.server_close()
+
+
+def main():
     server = HTTPServer(('127.0.0.1', 5000), ServiceHandler)
     server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
